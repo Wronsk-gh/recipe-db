a2f50ec3fd55a7fe8adc35437a7749770df738c4
wronsk <3601622+Wronsk-gh@users.noreply.github.com>
2023-10-29 18:23:36 +0000

    Removed Fuse implementation

--- src/components/RecipeTable.tsx
+++ src/components/RecipeTable.tsx
@@ -2,7 +2,6 @@ import '../App.css';
 import { Database } from 'firebase/database';
 import { Months, Ingredients, Recipes, Recipe } from '../db-types';
 import { RecipeRow } from './RecipeRow';
-import Fuse from 'fuse.js';
 
 export function RecipeTable({
   months,
@@ -18,9 +17,9 @@ export function RecipeTable({
   const rows = [];
   const headers = [];
   const fuseOptions = {
-    isCaseSensitive: false,
+    // isCaseSensitive: false,
     // includeScore: false,
-    shouldSort: true,
+    // shouldSort: true,
     // includeMatches: false,
     // findAllMatches: false,
     // minMatchCharLength: 1,
@@ -31,28 +30,21 @@ export function RecipeTable({
     // ignoreLocation: false,
     // ignoreFieldNorm: false,
     // fieldNormWeight: 1,
-    keys: ['recipe.name'],
+    keys: ['title', 'author.firstName'],
   };
 
   headers.push(<th key="name">Recipes</th>);
   headers.push(<th key="ingredients">Ingredients</th>);
   headers.push(<th key="months">Months</th>);
 
-  let recipesArray = Object.keys(recipes).map(function (key) {
-    return { id: key, recipe: recipes[key] };
-  });
-
-  const fuse = new Fuse(recipesArray, fuseOptions);
-  let filteredRecipes = fuse.search(filterText);
-
-  for (let i = 0; i < recipesArray.length; i++) {
+  for (const recipeId in recipes) {
     const recipe: Recipe = {
-      ...filteredRecipes[i].item.recipe,
-      recipeId: filteredRecipes[i].item.id,
+      ...recipes[recipeId],
+      recipeId: recipeId,
     };
     rows.push(
       <RecipeRow
-        key={recipe.recipeId}
+        key={recipeId}
         months={months}
         ingredients={ingredients}
         recipe={recipe}
@@ -60,46 +52,6 @@ export function RecipeTable({
     );
     // break;
   }
-  // if (filterText === '') {
-  //   for (const recipeId in recipes) {
-  //     const recipe: Recipe = {
-  //       ...recipes[recipeId],
-  //       recipeId: recipeId,
-  //     };
-  //     rows.push(
-  //       <RecipeRow
-  //         key={recipeId}
-  //         months={months}
-  //         ingredients={ingredients}
-  //         recipe={recipe}
-  //       />
-  //     );
-  //     // break;
-  //   }
-  // } else {
-  //   let recipesArray = Object.keys(recipes).map(function (key) {
-  //     return { id: key, recipe: recipes[key] };
-  //   });
-
-  //   const fuse = new Fuse(recipesArray, fuseOptions);
-  //   let filteredRecipes = fuse.search(filterText);
-
-  //   for (let i = 0; i < filteredRecipes.length; i++) {
-  //     const recipe: Recipe = {
-  //       ...filteredRecipes[i].item.recipe,
-  //       recipeId: filteredRecipes[i].item.id,
-  //     };
-  //     rows.push(
-  //       <RecipeRow
-  //         key={recipe.recipeId}
-  //         months={months}
-  //         ingredients={ingredients}
-  //         recipe={recipe}
-  //       />
-  //     );
-  //     // break;
-  //   }
-  // }
 
   return (
     <div>
