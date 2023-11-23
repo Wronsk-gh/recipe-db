b52ad42a3bd922a300af815df76fca020630cf67
wronsk <3601622+Wronsk-gh@users.noreply.github.com>
2023-11-23 20:19:08 +0000

    Disabled all queries for easier debugging

--- src/components/RecipeManager.tsx
+++ src/components/RecipeManager.tsx
@@ -20,42 +20,42 @@ export function RecipeManager() {
   const [currentView, setCurrentView] = useState<CurrentView>(
     CurrentView.recipes
   );
-  const {
-    isLoading: isMonthsLoading,
-    isError: isMonthsError,
-    data: monthsData,
-    error: monthsError,
-  } = useQuery({
-    queryKey: ['months'],
-    queryFn: async () => {
-      return await fetchMonths(db);
-    },
-    enabled: !!db,
-  });
-  const {
-    isLoading: isIngredientsLoading,
-    isError: isIngredientsError,
-    data: ingredientsData,
-    error: ingredientsError,
-  } = useQuery({
-    queryKey: ['ingredients'],
-    queryFn: async () => {
-      return await fetchIngredients(db);
-    },
-    enabled: !!db,
-  });
-  const {
-    isLoading: isRecipesLoading,
-    isError: isRecipesError,
-    data: recipesData,
-    error: recipesError,
-  } = useQuery({
-    queryKey: ['recipes'],
-    queryFn: async () => {
-      return await fetchRecipes(db);
-    },
-    enabled: !!db,
-  });
+  // const {
+  //   isLoading: isMonthsLoading,
+  //   isError: isMonthsError,
+  //   data: monthsData,
+  //   error: monthsError,
+  // } = useQuery({
+  //   queryKey: ['months'],
+  //   queryFn: async () => {
+  //     return await fetchMonths(db);
+  //   },
+  //   enabled: !!db,
+  // });
+  // const {
+  //   isLoading: isIngredientsLoading,
+  //   isError: isIngredientsError,
+  //   data: ingredientsData,
+  //   error: ingredientsError,
+  // } = useQuery({
+  //   queryKey: ['ingredients'],
+  //   queryFn: async () => {
+  //     return await fetchIngredients(db);
+  //   },
+  //   enabled: !!db,
+  // });
+  // const {
+  //   isLoading: isRecipesLoading,
+  //   isError: isRecipesError,
+  //   data: recipesData,
+  //   error: recipesError,
+  // } = useQuery({
+  //   queryKey: ['recipes'],
+  //   queryFn: async () => {
+  //     return await fetchRecipes(db);
+  //   },
+  //   enabled: !!db,
+  // });
 
   async function fetchThumbnail(googleId: string): Promise<string> {
     const response = await gapi.client.drive.files.get({
@@ -76,89 +76,89 @@ export function RecipeManager() {
     }
   }
 
-  const thumbnailsQueries = useQueries({
-    queries: (Object.entries(recipesData || {}) || []).map(
-      ([recipeId, recipe]) => {
-        return {
-          queryKey: ['thumbnail', recipe.google_id],
-          queryFn: async () => {
-            const recipeIdThumbnail: RecipesThumbnails = {};
-            recipeIdThumbnail[recipeId] = await fetchThumbnail(
-              recipe.google_id
-            );
-            return recipeIdThumbnail;
-          },
-          enabled: !!recipesData,
-          staleTime: 10 * 60 * 1000, // 10 minute
-        };
-      }
-    ),
-  });
+  // const thumbnailsQueries = useQueries({
+  //   queries: (Object.entries(recipesData || {}) || []).map(
+  //     ([recipeId, recipe]) => {
+  //       return {
+  //         queryKey: ['thumbnail', recipe.google_id],
+  //         queryFn: async () => {
+  //           const recipeIdThumbnail: RecipesThumbnails = {};
+  //           recipeIdThumbnail[recipeId] = await fetchThumbnail(
+  //             recipe.google_id
+  //           );
+  //           return recipeIdThumbnail;
+  //         },
+  //         enabled: !!recipesData,
+  //         staleTime: 10 * 60 * 1000, // 10 minute
+  //       };
+  //     }
+  //   ),
+  // });
 
-  const thumbnails: RecipesThumbnails = {};
+  // const thumbnails: RecipesThumbnails = {};
 
-  thumbnailsQueries.map((query) => {
-    if (query.data !== undefined) {
-      Object.assign(thumbnails, query.data);
-    }
-  });
+  // thumbnailsQueries.map((query) => {
+  //   if (query.data !== undefined) {
+  //     Object.assign(thumbnails, query.data);
+  //   }
+  // });
 
-  if (
-    monthsData !== undefined &&
-    ingredientsData !== undefined &&
-    recipesData !== undefined
-  ) {
-    // See which view to display
-    let view = <></>;
-    if (currentView === CurrentView.recipes) {
-      view = (
-        <FilterableRecipeTable
-          months={monthsData}
-          ingredients={ingredientsData}
-          recipes={recipesData}
-          recipesThumbnails={thumbnails}
-        />
-      );
-    } else if (currentView === CurrentView.ingredients) {
-      view = (
-        <FilterableIngredientTable
-          months={monthsData}
-          ingredients={ingredientsData}
-        />
-      );
-    }
+  // if (
+  //   monthsData !== undefined &&
+  //   ingredientsData !== undefined &&
+  //   recipesData !== undefined
+  // ) {
+  //   // See which view to display
+  //   let view = <></>;
+  //   // if (currentView === CurrentView.recipes) {
+  //   //   view = (
+  //   //     <FilterableRecipeTable
+  //   //       months={monthsData}
+  //   //       ingredients={ingredientsData}
+  //   //       recipes={recipesData}
+  //   //       recipesThumbnails={thumbnails}
+  //   //     />
+  //   //   );
+  //   // } else if (currentView === CurrentView.ingredients) {
+  //   //   view = (
+  //   //     <FilterableIngredientTable
+  //   //       months={monthsData}
+  //   //       ingredients={ingredientsData}
+  //   //     />
+  //   //   );
+  //   // }
 
-    return (
-      <>
-        <Auth setDb={setDb} />
-        <br />
-        <br />
-        <button
-          onClick={() => {
-            setCurrentView(CurrentView.recipes);
-          }}
-        >
-          Recipes
-        </button>
-        <button
-          onClick={() => {
-            setCurrentView(CurrentView.ingredients);
-          }}
-        >
-          Ingredients
-        </button>
-        <RtdbContext.Provider value={db}>
-          <br />
-          {view}
-        </RtdbContext.Provider>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Auth setDb={setDb} />
-        <p>Loading...</p>
-      </>
-    );
-  }
+  //   return (
+  //     <>
+  //       <Auth setDb={setDb} />
+  //       <br />
+  //       <br />
+  //       {/* <button
+  //         onClick={() => {
+  //           setCurrentView(CurrentView.recipes);
+  //         }}
+  //       >
+  //         Recipes
+  //       </button>
+  //       <button
+  //         onClick={() => {
+  //           setCurrentView(CurrentView.ingredients);
+  //         }}
+  //       >
+  //         Ingredients
+  //       </button>
+  //       <RtdbContext.Provider value={db}>
+  //         <br />
+  //         {view}
+  //       </RtdbContext.Provider> */}
+  //     </>
+  //   );
+  // } else {
+  return (
+    <>
+      <Auth setDb={setDb} />
+      <p>Loading...</p>
+    </>
+  );
+  // }
 }
