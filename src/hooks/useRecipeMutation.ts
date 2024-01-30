b7b08bbc444e8e9803645a261dbb39e05c923d4a
wronsk <3601622+Wronsk-gh@users.noreply.github.com>
2024-01-30 15:25:47 +0000

    feat: Fix import, add cancelQueries and setQueryData and remove commented code

--- src/hooks/useRecipeMutation.ts
+++ src/hooks/useRecipeMutation.ts
@@ -2,7 +2,8 @@ import { useContext } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { RtdbContext } from '../components/RtdbContext';
 import { updateRecipeDisplayUserDb } from '../rtdb';
-import { Recipe } from '../db-types';
+import { Recipe, RecipesDb } from '../db-types';
+import { getRecipeDbRepr } from '../models/RecipeUtils';
 
 export function useRecipeMutation() {
   // Get QueryClient from the context
@@ -10,23 +11,42 @@ export function useRecipeMutation() {
   // Get the Rtdb from the context
   const rtdbCred = useContext(RtdbContext);
 
-  const recipeMutation = useMutation({
+  const recipeMutation = useMutation<RecipesDb, any, Recipe, RecipesDb>({
+    // When mutate is called:
     mutationFn: async (newRecipe: Recipe) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: ['recipes'] });
+
+      // Snapshot the previous value
+      const previousRecipesDb = queryClient.getQueryData(['recipes']);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData<RecipesDb>(['recipes'], (oldDb) => {
+        const newRecipeDb = getRecipeDbRepr(newRecipe);
+        return { ...oldDb, [newRecipe.id]: newRecipeDb };
+      });
+
       await updateRecipeDisplayUserDb(rtdbCred, newRecipe);
+
+      return { previousRecipesDb };
     },
-    onError: () => {
+    onError: (err, newRecipe, context) => {
+      queryClient.setQueryData(['recipes'], context.previousRecipesDb);
       window.alert('Could not update...');
     },
-    onSuccess: onRecipeMutationSuccess,
+    // onSuccess: onRecipeMutationSuccess,
     onSettled: () => {
       recipeMutation.reset();
+      // Always refetch after error or success:
+      queryClient.invalidateQueries({ queryKey: ['recipes'] });
     },
   });
 
-  function onRecipeMutationSuccess() {
-    // Force an update of the recipes
-    queryClient.invalidateQueries({ queryKey: ['recipes'] });
-  }
+  // function onRecipeMutationSuccess() {
+  //   // Force an update of the recipes
+  //   queryClient.invalidateQueries({ queryKey: ['recipes'] });
+  // }
 
   return recipeMutation;
 }
